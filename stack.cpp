53c614a39c8b58136aef7cb11e2c62e52aeb897a
muhammadridho-debug <idomuhammad11@gmail.com>
2026-05-14 15:01:31 +0700

    Menambahkan class Node

--- stack.cpp
+++ stack.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 using namespace std;
 
+class Node {
+public:
+    int data;
+    Node *next;
+
+    Node() {
+        next = NULL;
+    }
+};
+
+
 int main() {
     return 0;
 }
